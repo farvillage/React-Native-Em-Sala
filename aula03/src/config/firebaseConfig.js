55ca9e9da05d7ce0363d6efdf56e4a6042934935
farvillage <ygorsoares2003@gmail.com>
2025-06-05 17:54:14 -0300

    config in firebaseocnfig

--- aula03/src/config/firebaseConfig.js
+++ aula03/src/config/firebaseConfig.js
@@ -1,6 +1,6 @@
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 import { 
 API_KEY,
@@ -21,7 +21,7 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-const db = getFirestore(app)
 const auth = getAuth(app)
+const db = getFirestore(app)
 
 export { db, auth };
